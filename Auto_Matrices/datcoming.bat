a788b5fb14f71412ce498e25d654f9ac21344a85
lucafc <39124944+lucafc@users.noreply.github.com>
2018-10-01 14:36:09 +0200

    Auto_matrices.m

--- Auto_Matrices/datcoming.bat
+++ Auto_Matrices/datcoming.bat
@@ -1,6 +1,3 @@
 @echo off
 
-cd C:\Stability_Opt\datcom
 start datcom.exe
-
-cd C:\Stability_Opt
--- Auto_Matrices/parsing.bat
+++ Auto_Matrices/parsing.bat
@@ -1,6 +1,3 @@
 @echo off
 
-cd C:\Stability_Opt\parser
 start datcom_parser.py
-
-cd C:\Stability_Opt
